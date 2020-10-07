ccfdb971b8697cfd5676d0c5cb3342dd350382e5
chulongc <chen.chulong@live.com>
2020-10-07 12:09:03 -0700

    use deformable conv ops from mmcv instead of stock to support pytorch1.6

--- det3d/ops/dcn/__init__.py
+++ det3d/ops/dcn/__init__.py
@@ -1,6 +1,12 @@
-from .deform_conv import (DeformConv, DeformConvPack, ModulatedDeformConv,
-                          ModulatedDeformConvPack, deform_conv,
-                          modulated_deform_conv)
+# from .deform_conv import (DeformConv, DeformConvPack, ModulatedDeformConv,
+#                           ModulatedDeformConvPack, deform_conv,
+#                           modulated_deform_conv)
+from mmcv.ops import DeformConv2d as DeformConv
+from mmcv.ops import DeformConv2dPack as DeformConvPack
+from mmcv.ops import ModulatedDeformConv2dPack as ModulatedDeformConvPack
+from mmcv.ops import ModulatedDeformConv2d as ModulatedDeformConv
+from mmcv.ops import deform_conv2d as deform_conv
+from mmcv.ops import modulated_deform_conv2d as modulated_deform_conv
 
 __all__ = [
     'DeformConv', 'DeformConvPack', 'ModulatedDeformConv',
